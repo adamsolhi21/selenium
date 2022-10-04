ec51955efa5079087c913b7631befb8a6e4a6d89
AdamTravers <83338326+adamsolhi21@users.noreply.github.com>
2022-10-04 11:03:01 +0100

    Add files via upload

---
+++ tests.js
@@ -0,0 +1,37 @@
+const {Builder, By, Key} = require("selenium-webdriver");
+const assert = require ("assert");
+
+
+async function example(){
+
+//launch browser
+let driver = await new Builder().forBrowser("chrome").build();
+
+//navigate to application
+await driver.get("https://lambdatest.github.io/sample-todo-app/") 
+//https://stg-com.lh.uk/
+
+//edit page
+await driver.findElement(By.id("sampletodotext")).sendKeys("kljflkd", Key.RETURN);
+
+//assert
+let todoText = await driver.findElement(By.xpath("//li[last()]")).getText().then(function(value){
+    return value
+});
+
+
+//node assertions
+assert.strictEqual(todoText,"kljflkd");
+
+//chai assertions
+
+
+
+
+//close browser
+await driver.quit();
+
+
+}
+
+example()
