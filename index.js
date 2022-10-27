49c83e4f099850f96336d4ab84b09403a3d639fe
AdamTravers <83338326+adamsolhi21@users.noreply.github.com>
2022-10-27 16:34:30 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,96 @@
+/*
+    LambdaTest selenium automation sample example
+    Configuration
+    ----------
+    username: Username can be found at automation dashboard
+    accessToken:  AccessToken can be generated from automation dashboard or profile section
+
+    Result
+    -------
+    Execute NodeJS Automation Tests on LambdaTest Distributed Selenium Grid 
+*/
+const webdriver = require('selenium-webdriver');
+
+/*
+    Setup remote driver
+    Params
+    ----------
+    platform : Supported platform - (Windows 10, Windows 8.1, Windows 8, Windows 7,  macOS High Sierra, macOS Sierra, OS X El Capitan, OS X Yosemite, OS X Mavericks)
+    browserName : Supported platform - (chrome, firefox, Internet Explorer, MicrosoftEdge, Safari)
+    version :  Supported list of version can be found at https://www.lambdatest.com/capabilities-generator/
+*/
+
+// username: Username can be found at automation dashboard
+const USERNAME = process.env.LT_USERNAME;
+
+// AccessKey:  AccessKey can be generated from automation dashboard or profile section
+const KEY = process.env.LT_ACCESS_KEY;
+
+// gridUrl: gridUrl can be found at automation dashboard
+const GRID_HOST = 'hub.lambdatest.com/wd/hub';
+
+
+function searchTextOnGoogle() {
+
+    // Setup Input capabilities
+    /*const capabilities = {
+        platform: 'windows 10',
+        browserName: 'chrome',
+        version: 'latest',
+        resolution: '1280x800',
+        // geoLocation : "US",
+        // network: true,
+        // visual: true,
+        // console: true,
+        // video: true,
+        name: 'Test 1', // name of the test
+        build: 'NodeJS build' // name of the build
+    }*/
+
+    const capabilities = {
+        "browserName": "Chrome",
+        "browserVersion": "106.0",
+        "LT:Options": {
+           // "username": "adam.travers",
+           // "accessKey": "3QvTjiaQwdjoXJbYH7y6EospP3nrc1NQ3I24tlqqrhyU6yKxAa",
+            "platformName": "Windows 10",
+            "project": "Untitled",
+            "w3c": true,
+            "plugin": "node_js-node_js"
+        }
+    }
+
+    // URL: https://{username}:{accessToken}@beta-hub.lambdatest.com/wd/hub
+    const gridUrl = 'https://' + USERNAME + ':' + KEY + '@' + GRID_HOST;
+
+    // setup and build selenium driver object 
+    const driver = new webdriver.Builder()
+        .usingServer(gridUrl)
+        .withCapabilities(capabilities)
+        .build();
+
+        
+
+    // navigate to a url, click on the first and second list items and add a new one in the list.
+    driver.get('https://lambdatest.github.io/sample-todo-app/').then(function() {
+        driver.findElement(webdriver.By.name('li1')).click().then(function(){
+           console.log("Successfully clicked first list item.");
+        });
+        driver.findElement(webdriver.By.name('li2')).click().then(function(){
+            console.log("Successfully clicked second list item.");
+         });
+
+         driver.findElement(webdriver.By.id('sampletodotext')).sendKeys('Complete Lambdatest Tutorial\n').then(function(){
+             driver.findElement(webdriver.By.id('addbutton')).click().then(function(){
+                 console.log("Successfully added a new task.");
+             })
+         });
+
+    }).catch(function(err){
+        console.log("test failed with reason "+err)
+        driver.executeScript('lambda-status=failed');
+        driver.quit();
+    });
+}
+searchTextOnGoogle();
+
